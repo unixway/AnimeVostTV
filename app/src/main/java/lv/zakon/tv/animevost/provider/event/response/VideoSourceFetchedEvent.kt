435937d2b5e505151b36b045d1374a65b2c223cc
Pasha <zakon@inbox.lv>
2024-12-21 22:27:46 +0200

    добавлен запрос за альтернативным видеоисточником

---
+++ app/src/main/java/lv/zakon/tv/animevost/provider/event/response/VideoSourceFetchedEvent.kt
@@ -0,0 +1,3 @@
+package lv.zakon.tv.animevost.provider.event.response
+
+class VideoSourceFetchedEvent(val movieId: Long, val videoSource: String)
--- app/src/main/java/lv/zakon/tv/animevost/ui/playback/PlaybackVideoFragment.kt
+++ app/src/main/java/lv/zakon/tv/animevost/ui/playback/PlaybackVideoFragment.kt
@@ -8,15 +8,23 @@ import androidx.leanback.app.VideoSupportFragmentGlueHost
 import androidx.leanback.media.MediaPlayerAdapter
 import androidx.leanback.widget.PlaybackControlsRow
 import androidx.lifecycle.lifecycleScope
+import io.ktor.util.reflect.instanceOf
 import kotlinx.coroutines.launch
 import lv.zakon.tv.animevost.R
 import lv.zakon.tv.animevost.model.MovieSeriesPageInfo
 import lv.zakon.tv.animevost.model.PlayEntry
 import lv.zakon.tv.animevost.prefs.AppPrefs
 import lv.zakon.tv.animevost.provider.AnimeVostProvider
+import lv.zakon.tv.animevost.provider.RequestId
+import lv.zakon.tv.animevost.provider.event.request.EventCounterGenerator
+import lv.zakon.tv.animevost.provider.event.response.MovieSeriesFetchedEvent
+import lv.zakon.tv.animevost.provider.event.response.VideoSourceFetchedEvent
 import lv.zakon.tv.animevost.ui.common.Util
 import lv.zakon.tv.animevost.ui.common.Util.IfExt.ifData
 import lv.zakon.tv.animevost.ui.detail.DetailsActivity
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 /** Handles video playback with media controls. */
 class PlaybackVideoFragment : VideoSupportFragment() {
@@ -24,6 +32,7 @@ class PlaybackVideoFragment : VideoSupportFragment() {
     private lateinit var mTransportControlGlue : VideoPlayerGlue
     private lateinit var movieSeriesPageInfo : MovieSeriesPageInfo
     private lateinit var videoDesc : PlayEntry
+    private lateinit var playerAdapter : MediaPlayerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +40,8 @@ class PlaybackVideoFragment : VideoSupportFragment() {
         movieSeriesPageInfo = Util.getExtra(activity!!, DetailsActivity.MOVIE_SERIES_DETAILS)
         videoDesc = Util.getExtra(activity!!, DetailsActivity.PLAY_DESC)
 
-        val videoUrl = AnimeVostProvider.instance.getMovieSource(videoDesc.id)
-
         val glueHost = VideoSupportFragmentGlueHost(this@PlaybackVideoFragment)
-        val playerAdapter = MediaPlayerAdapter(context)
+        playerAdapter = MediaPlayerAdapter(context)
         playerAdapter.setRepeatAction(PlaybackControlsRow.RepeatAction.INDEX_NONE)
 
         mTransportControlGlue = VideoPlayerGlue(context, playerAdapter)
@@ -43,20 +50,35 @@ class PlaybackVideoFragment : VideoSupportFragment() {
         mTransportControlGlue.subtitle = getString(R.string.playback_info, movieSeriesPageInfo.info.yearStart.toString() + movieSeriesPageInfo.info.yearEnd.ifData("") { "-$it" }, movieSeriesPageInfo.info.genres.contentToString())
         mTransportControlGlue.isSeekEnabled = true
 
-        playerAdapter.setDataSource(Uri.parse(videoUrl))
-        Log.i("Player", "storedPosition: ${videoDesc.storedPosition}")
-        if (videoDesc.storedPosition > 0) {
-            mTransportControlGlue.seekToAndPlayWhenPrepared(videoDesc.storedPosition)
-        } else {
-            mTransportControlGlue.playWhenPrepared()
-        }
-
         lifecycleScope.launch {
-            AppPrefs.markWatch(movieSeriesPageInfo.info.id, movieSeriesPageInfo.info.pageUrl, videoDesc.id)
+            AnimeVostProvider.instance.requestAlternativeVideoSource(videoDesc.id)
         }
     }
 
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onEvent(event : VideoSourceFetchedEvent) {
+        if (videoDesc.id == event.movieId) {
+            playerAdapter.setDataSource(Uri.parse(event.videoSource))
+            Log.i("Player", "storedPosition: ${videoDesc.storedPosition}")
+            if (videoDesc.storedPosition > 0) {
+                mTransportControlGlue.seekToAndPlayWhenPrepared(videoDesc.storedPosition)
+            } else {
+                mTransportControlGlue.playWhenPrepared()
+            }
+
+            lifecycleScope.launch {
+                AppPrefs.markWatch(movieSeriesPageInfo.info.id, movieSeriesPageInfo.info.pageUrl, videoDesc.id)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        EventBus.getDefault().register(this)
+    }
+
     override fun onPause() {
+        EventBus.getDefault().unregister(this)
         super.onPause()
         mTransportControlGlue.pause()
         Log.i("VideoPlayer", "onPause: ${mTransportControlGlue.currentPosition}/${mTransportControlGlue.duration}")
@@ -68,4 +90,6 @@ class PlaybackVideoFragment : VideoSupportFragment() {
             }
         }
     }
+
+
 }
